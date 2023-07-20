2e28d11d8735d8351d8ed068403d82ef61447009
Matias Lenci <matiaslenci@gmail.com>
2023-07-20 00:16:31 -0300

    add card de subscripciones

---
+++ src/app/modules/home/components/cards/cards.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.scss'],
+})
+export class CardsComponent {}
