d3cc31886219f05737e78f96ea2456fca9e44ff4
Matias Lenci <matiaslenci@gmail.com>
2023-07-20 20:58:15 -0300

    add router module, add pagina para ver subscripciones en detalle

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HomePageComponent, CardsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HomePageComponent],
 })
 export class HomeModule {}
---
+++ src/app/modules/subscripcion/subscripcion.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SubsPageComponent } from './pages/subs-page/subs-page.component';
+
+@NgModule({
+  declarations: [SubsPageComponent],
+  imports: [CommonModule],
+  exports: [SubsPageComponent],
+})
+export class SubscripcionModule {}
