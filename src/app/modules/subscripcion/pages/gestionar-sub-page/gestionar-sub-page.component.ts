83a704c91fd15aed750c703a4aa58d36ec2c42ab
Matias Lenci <matiaslenci@gmail.com>
2023-07-24 22:41:32 -0300

    add gestionar sub page

--- src/app/modules/subscripcion/pages/gestionar-sub-page/gestionar-sub-page.component.ts
+++ src/app/modules/subscripcion/pages/gestionar-sub-page/gestionar-sub-page.component.ts
@@ -2,8 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './gestionar-sub-page.component.html',
-  styleUrls: ['./gestionar-sub-page.component.scss']
+  styleUrls: ['./gestionar-sub-page.component.scss'],
 })
 export class GestionarSubPageComponent {
+  editIcon() {}
+  /**
+   * Bool que inidica si ya agrego amigos a la subscripcion
+   * Modifica el boton de compartir subscripción por otro si es true
+   */
+  compartido: boolean = false;
 
+  /**
+   * Oculta o muestra la contraseña
+   */
+  hidePassword: boolean = true;
+
+  //TODO: Traer desde rest
+  password = 'contraseña123';
+  /**
+   * Esta función se llama cuando se hace clic en el SVG.
+   * Cambia el valor de hidePassword al valor opuesto de su estado actual.
+   * Si hidePassword es false, se cambiará a true.
+   */
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
 }
