2b7e4f12be7dd13f3c035c7baa794bcc9d41641e
Matias Lenci <matiaslenci@gmail.com>
2023-07-25 20:39:10 -0300

    add new Sub page, add var edit mode

---
+++ src/app/modules/nueva-sub/components/card-new-sub/card-new-sub.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-new-sub',
+  templateUrl: './card-new-sub.component.html',
+  styleUrls: ['./card-new-sub.component.scss']
+})
+export class CardNewSubComponent {
+
+}
