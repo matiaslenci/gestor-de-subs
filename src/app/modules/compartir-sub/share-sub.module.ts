c0bbcdcffc6e11c452b575dfcfdbbbe1f6480e3b
Matias Lenci <matiaslenci@gmail.com>
2023-07-29 10:13:10 -0300

    add lista de subscripciones

--- src/app/modules/compartir-sub/share-sub.module.ts
+++ src/app/modules/compartir-sub/share-sub.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NewShareSubPageComponent } from './pages/new-share-sub-page/new-share-sub-page.component';
 import { ListShareSubPageComponent } from './pages/list-share-sub-page/list-share-sub-page.component';
 import { ShareSubRoutingModule } from './share-sub-routing.module';
+import { CardAmigosComponent } from './components/card-amigos/card-amigos.component';
 
 @NgModule({
-  declarations: [NewShareSubPageComponent, ListShareSubPageComponent],
+  declarations: [NewShareSubPageComponent, ListShareSubPageComponent, CardAmigosComponent],
   imports: [CommonModule, ShareSubRoutingModule],
   exports: [NewShareSubPageComponent, ListShareSubPageComponent],
 })
