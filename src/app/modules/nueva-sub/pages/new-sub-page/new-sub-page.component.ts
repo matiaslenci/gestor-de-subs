0cd4e73af585fc2f2a59dd97830b4d675e21c19f
Matias Lenci <matiaslenci@gmail.com>
2023-07-29 12:21:00 -0300

    add lista de subscripciones a nueva subcripcion

--- src/app/modules/nueva-sub/pages/new-sub-page/new-sub-page.component.ts
+++ src/app/modules/nueva-sub/pages/new-sub-page/new-sub-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SubscripcionesService } from 'src/app/shared/services/subscripciones.service';
 
 @Component({
   templateUrl: './new-sub-page.component.html',
-  styleUrls: ['./new-sub-page.component.scss']
+  styleUrls: ['./new-sub-page.component.scss'],
 })
 export class NewSubPageComponent {
-
+  constructor(public subsSrv: SubscripcionesService) {}
 }
